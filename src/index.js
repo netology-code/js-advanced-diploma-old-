de1ed23b7f2985e1aa638369e434af19c387444e
Denis Nikitin <dinya.nikitin@gmail.com>
2021-07-25 21:23:26 +0300

    init 0.0.1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './css/style.css';
 import './js/app';
+import './img';
 
 // entry point for webpack
 // don't write your code here
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,16 @@ module.exports = {
         use: {
           loader: 'babel-loader',
         },
+      }, {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
       },
       {
         test: /\.html$/,
