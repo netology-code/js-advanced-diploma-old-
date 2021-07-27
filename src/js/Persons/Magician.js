7b500eca657f47242c09add4ab4238fd62e5e6c9
Denis Nikitin <dinya.nikitin@gmail.com>
2021-07-27 15:16:50 +0300

    add Persons

---
+++ src/js/Persons/Magician.js
@@ -0,0 +1,9 @@
+import Character from '../Character';
+
+export default class Magician extends Character {
+  constructor(level) {
+    super(level, 'magician');
+    this.attack = 10;
+    this.defence = 40;
+  }
+}
